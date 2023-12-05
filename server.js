7d48459f4b3cc6b9772b929db256b063d834dbb2
RonaldinhoCc <ronaldinho.ccencho@vallegrande.edu.pe>
2023-12-05 13:37:53 -0500

    sesion.html

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const mysql = require('mysql');
+const session = require('express-session');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+
 
 let conexion = mysql.createConnection({
     host: 'localhost',
@@ -31,7 +33,22 @@ app.use("/img",express.static(__dirname +'/form/img'));
 app.get("/",function(req,res){ 
     var filePath = path.join(__dirname, '/form/index.html');
     res.sendFile(filePath);
-})
+});
+app.get("/index.html",function(req,res){ 
+    var filePath = path.join(__dirname, '/form/index.html');
+    res.sendFile(filePath);
+});
+
+app.get("/sesion.html",function(req,res){ 
+    var filePath = path.join(__dirname, '/form/sesion.html');
+    res.sendFile(filePath);
+});
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 app.post("/validar", (req, res)=>{
     let data= req.body;
@@ -54,6 +71,40 @@ app.post("/validar", (req, res)=>{
         }
       });
 });
+
+// login 
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+// Execute SQL query that'll select the account from the database based on the specified username and password
+		conexion.query('SELECT * FROM estudiante WHERE nombres = ? AND password = ?', [username, password], function(error, results, fields) {
+            console.log("Consulta de usuario en progreso")
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+                console.log("Inicio de sesión exitoso para usuario: " + username);
+				// Redirect to home page
+				response.redirect('/index.html');
+			} else {
+                console.log("Usuario y/o Contraseña Incorrecta");
+				response.send('Usuario y/o Contraseña Incorrecta');
+			}			
+			response.end();
+		});
+	} else {
+        console.log('Por favor ingresa Usuario y Contraseña!');
+		response.send('Por favor ingresa Usuario y Contraseña!');
+		response.end();
+	}
+});
+
 
 
 
